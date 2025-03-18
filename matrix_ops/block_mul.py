421e5059e5a0cb20979495ce23af530bec4c4b3c
mnozkiewicz <michno513@gmail.com>
2025-03-18 23:54:42 +0100

    Implemented agorithms and bindings

---
+++ matrix_ops/block_mul.py
@@ -0,0 +1,33 @@
+import numpy as np
+from .matrix_module import _binet, _strassen, _hybrid, Matrix
+
+def is_power_of_two(n: int) -> bool:
+    return n > 0 and (n & (n - 1)) == 0
+
+
+def check_matrices(mat1: Matrix, mat2: Matrix):
+    m1, n1 = mat1.getHeight(), mat1.getWidth()
+    m2, n2 = mat2.getHeight(), mat2.getWidth()
+
+    if m1 != n1 or m2 != n2:
+        raise ValueError("Matrices should be squared")
+    
+    if m1 != m2:
+        raise ValueError("Matrices shapes do not match")
+    
+    if not is_power_of_two(m1):
+        raise ValueError("Matrix size should be a power of two")
+
+
+def binet(mat1: Matrix, mat2: Matrix) -> Matrix:
+    check_matrices(mat1, mat2)
+    return _binet(mat1, mat2)
+
+
+def strassen(mat1: Matrix, mat2: Matrix) -> Matrix:
+    check_matrices(mat1, mat2)
+    return _strassen(mat1, mat2)
+
+def hybrid(mat1: Matrix, mat2: Matrix) -> Matrix:
+    check_matrices(mat1, mat2)
+    return _hybrid(mat1, mat2)
---
+++ src/bindings.cpp
@@ -0,0 +1,66 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
+#include <iostream>
+#include <vector>
+#include "matrix.h"
+#include "multiplications.h"
+
+namespace py = pybind11;
+
+py::array_t<double> matrixToNumpy(const Matrix& matrix){
+
+    int height = matrix.getHeight();
+    int width = matrix.getWidth();
+
+    py::array_t<double> arr({height, width});
+    py::buffer_info buf = arr.request();
+
+    double* ptr = static_cast<double*>(buf.ptr);
+
+    for (int i = 0; i < height; ++i) {
+        for(int j = 0; j < width; ++j){
+            ptr[i*width + j] = matrix(i, j);
+        }
+    }
+    return arr;
+}
+
+Matrix numpyToMatrix(py::array_t<double>& numpyArr){
+
+    py::buffer_info buf = numpyArr.request();
+    double* ptr = static_cast<double*>(buf.ptr);
+
+    if (buf.ndim != 2) {
+        throw std::runtime_error("Input numpy array should be two dimensional");
+    }
+
+    int height = buf.shape[0];
+    int width = buf.shape[1];
+
+    return Matrix(height, width, ptr);
+}
+
+PYBIND11_MODULE(matrix_module, m) {
+    m.def("createRandomMatrix", &createRandomMatrix, "A function that adds two numbers");
+    m.def("matrixToNumpy", &matrixToNumpy, "Convert Matrix object to numpy");
+    m.def("numpyToMatrix", &numpyToMatrix, "Convert Numpy object to Matrix");
+
+    m.def("_binet", &binet, "Binet matrix multiplication");
+    m.def("_strassen", &strassen, "Strassen matrix multiplication");
+    m.def("_hybrid", &hybrid, "Hybrid matrix multiplication");
+
+    py::class_<Matrix>(m, "Matrix")
+        .def(py::init<int, int, double>())  // Bind constructor
+        .def("getHeight", &Matrix::getHeight)  // Bind method
+        .def("getWidth", &Matrix::getWidth)
+        .def("print", &Matrix::print)
+        .def("at", py::overload_cast<int, int>(&Matrix::operator(), py::const_))
+        .def("slice", &Matrix::slice)
+        .def("__add__", py::overload_cast<const Matrix&>(&Matrix::operator+, py::const_))
+        .def("__add__", py::overload_cast<double>(&Matrix::operator+, py::const_))
+        .def("__sub__", py::overload_cast<const Matrix&>(&Matrix::operator-, py::const_))
+        .def("__sub__", py::overload_cast<double>(&Matrix::operator-, py::const_))
+        .def("__mul__", py::overload_cast<const Matrix&>(&Matrix::operator*, py::const_))
+        .def("__mul__", py::overload_cast<double>(&Matrix::operator*, py::const_))
+        .def("__matmul__", &Matrix::operator^);
+}
