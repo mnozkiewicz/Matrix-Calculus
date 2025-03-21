5f3967885105fcd6e76a13f32d01a4f530c2524c
Jakub Kasperski <k2002kuba@gmail.com>
2025-03-21 23:26:33 +0100

    Build_lib.sh is now universal

--- build_lib.sh
+++ build_lib.sh
@@ -2,4 +2,4 @@ cmake -S . -B build_cpp -DPython3_EXECUTABLE=$(which python)
 cmake --build build_cpp
 
 mkdir -p matrix_ops
-cp build_cpp/matrix_module.cpython-312-darwin.so matrix_ops/
+cp build_cpp/matrix_module.cpython*.so matrix_ops/
